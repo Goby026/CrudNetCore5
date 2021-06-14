738153129dc767691056a99ceee7bc3395f9c494
Grover Rendich <george.rendich@gmail.com>
2021-06-13 21:57:38 -0500

    Agregar archivos de proyecto.

---
+++ CrudNetCore5/Controllers/LibrosController.cs
@@ -0,0 +1,108 @@
+﻿using CrudNetCore5.Data;
+using CrudNetCore5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Http Get Index
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> libros = _context.Libro ;
+            return View(libros);
+        }
+
+        //Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Http Post Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Libro libro)
+        {
+            if (ModelState.IsValid) {
+                _context.Libro.Add(libro);
+                _context.SaveChanges();
+
+                TempData["msg"] = "El libro se registró correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //Http get Edit
+        public IActionResult Edit(int? id) {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if ( libro == null ) {
+                return NotFound();
+            }
+
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Libro.Update(libro);
+                _context.SaveChanges();
+
+                TempData["msg"] = "El libro se actualizó correctamente";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        //Http get Edit
+        public IActionResult Delete(int? id)
+        {
+            
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var libro = _context.Libro.Find(id);
+
+            if (libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libro.Remove(libro);
+            _context.SaveChanges();
+
+            TempData["msg"] = $"El libro {id} se eliminó correctamente";
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ CrudNetCore5/Models/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es requerido")]
+        [StringLength(50, ErrorMessage ="El {0} debe ser almenos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Título")]
+        public String Titulo { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [StringLength(200, ErrorMessage = "El {0} debe ser almenos {2} y máximo {1} caracteres", MinimumLength = 3)]
+        [Display(Name = "Descripción")]
+        public String Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La fecha es requerida")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "El autor es requerido")]
+        public String Autor { get; set; }
+
+        [Required(ErrorMessage = "El precio es requerido")]
+        public double Precio { get; set; }
+    }
+}
